9810037df929f2a3a68d920da9a758298fdc257f
mohit <mohit.gangil2012@gmail.com>
2021-05-11 13:31:46 +0530

    Added basic commit

---
+++ src/main/java/com/javatpoint/App.java
@@ -0,0 +1,10 @@
+package com.javatpoint;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Comes on ");
+    }
+}
